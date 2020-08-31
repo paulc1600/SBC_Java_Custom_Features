8dd73a542efb73726ed871753fca6149977a1291
paulc1600 <ctmtest2304@gmail.com>
2020-08-31 16:57:07 -0700

    Day 10 changes

---
+++ src/main/java/TwoIntegerSum.java
@@ -0,0 +1,12 @@
+package aut;
+
+public class TwoIntegerSum {
+    static public int fint = 0;
+    static public int sint = 0;
+
+    public static int main() {
+        int mySum = fint + sint;
+        System.out.println("TIS: The sum of " + fint + " and " + sint + " = " + mySum);
+        return mySum;
+    }
+}
