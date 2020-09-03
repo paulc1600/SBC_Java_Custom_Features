4066162d0d2ea5e3f0a1a69bb75bf27b6b5b8121
paulc1600 <ctmtest2304@gmail.com>
2020-09-02 17:23:37 -0700

    Day 11 Outline Code

--- src/test/java/definitions/CalculatorNetSteps.java
+++ src/test/java/definitions/CalculatorNetSteps.java
@@ -4,9 +4,14 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
+import static definitions.ATestToolBox.getWait;
+import static org.junit.Assert.assertTrue;
+import static org.openqa.selenium.support.ui.ExpectedConditions.elementToBeClickable;
 import static support.TestContext.getDriver;
 
 public class CalculatorNetSteps {
@@ -97,5 +102,144 @@ public class CalculatorNetSteps {
             System.out.println("================================================");
         }
         org.junit.Assert.assertEquals(checkThreshCompare, true);
+    }
+
+    // -----------------------------------------------------------------
+    //  @calculatorOutlineTests -- I start their scientific calculator
+    // -----------------------------------------------------------------
+    @When("I start their scientific calculator")
+    public void iStartTheirScientificCalculator() {
+        getDriver().findElement(By.xpath("//a[contains(text(),'Scientific Calculator')]")).click();
+    }
+
+    // -----------------------------------------------------------------
+    //  @calculatorOutlineTests -- Enter 1st number
+    // -----------------------------------------------------------------
+    @And("I enter first number {string}")
+    public void iEnterFirstNumber(String nbr1) {
+        WebDriverWait calcAppears = getWait();
+        calcAppears.until(elementToBeClickable(By.xpath("//span[contains(text(),'AC')]")));
+        WebElement K0 = getDriver().findElement(By.xpath("//span[@class='scinm'][contains(text(),'0')]"));
+        WebElement K1 = getDriver().findElement(By.xpath("//span[@class='scinm'][contains(text(),'1')]"));
+        WebElement K2 = getDriver().findElement(By.xpath("//span[contains(text(),'2')]"));
+        WebElement K3 = getDriver().findElement(By.xpath("//span[contains(text(),'3')]"));
+        WebElement K4 = getDriver().findElement(By.xpath("//span[contains(text(),'4')]"));
+        WebElement K5 = getDriver().findElement(By.xpath("//span[contains(text(),'5')]"));
+        WebElement K6 = getDriver().findElement(By.xpath("//span[contains(text(),'6')]"));
+        WebElement K7 = getDriver().findElement(By.xpath("//span[contains(text(),'7')]"));
+        WebElement K8 = getDriver().findElement(By.xpath("//span[contains(text(),'8')]"));
+        WebElement K9 = getDriver().findElement(By.xpath("//span[contains(text(),'9')]"));
+        WebElement Kdp = getDriver().findElement(By.xpath("//span[contains(text(),'.')]"));
+        WebElement Kac = getDriver().findElement(By.xpath("//span[contains(text(),'AC')]"));
+
+        Kac.click();     //reset calculator
+
+        for (int i = 0; i < nbr1.length(); i++){
+            char c = nbr1.charAt(i);
+            String key = String.valueOf(c);
+
+            switch (key) {
+                case "0" -> K0.click();
+                case "1" -> K1.click();
+                case "2" -> K2.click();
+                case "3" -> K3.click();
+                case "4" -> K4.click();
+                case "5" -> K5.click();
+                case "6" -> K6.click();
+                case "7" -> K7.click();
+                case "8" -> K8.click();
+                case "9" -> K9.click();
+                case "." -> Kdp.click();
+                case "C" -> Kac.click();
+                default -> throw new IllegalStateException("Error: This key name is invalid: " + key);
+            }
+        }
+    }
+
+    // -----------------------------------------------------------------
+    //  @calculatorOutlineTests -- Enter Math Function
+    // -----------------------------------------------------------------
+    @And("I enter the function {string}")
+    public void iEnterTheFunction(String func) throws InterruptedException {
+        WebElement Kadd = getDriver().findElement(By.xpath("//span[contains(@onclick,\"r('+')\")]"));
+        WebElement Ksub = getDriver().findElement(By.xpath("//span[contains(@onclick,\"r('-')\")]"));
+        WebElement Kac = getDriver().findElement(By.xpath("//span[contains(text(),'AC')]"));
+
+        switch (func) {
+            case "+" -> Kadd.click();
+            case "-" -> Ksub.click();
+            case "C" -> Kac.click();
+            default -> throw new IllegalStateException("Error: This key name is invalid: " + func);
+        }
+    }
+
+    // -----------------------------------------------------------------
+    //  @calculatorOutlineTests -- Enter 1st number
+    // -----------------------------------------------------------------
+    @And("I enter the second number {string}")
+    public void iEnterTheSecondNumber(String nbr2) {
+        WebElement K0 = getDriver().findElement(By.xpath("//span[@class='scinm'][contains(text(),'0')]"));
+        WebElement K1 = getDriver().findElement(By.xpath("//span[@class='scinm'][contains(text(),'1')]"));
+        WebElement K2 = getDriver().findElement(By.xpath("//span[contains(text(),'2')]"));
+        WebElement K3 = getDriver().findElement(By.xpath("//span[contains(text(),'3')]"));
+        WebElement K4 = getDriver().findElement(By.xpath("//span[contains(text(),'4')]"));
+        WebElement K5 = getDriver().findElement(By.xpath("//span[contains(text(),'5')]"));
+        WebElement K6 = getDriver().findElement(By.xpath("//span[contains(text(),'6')]"));
+        WebElement K7 = getDriver().findElement(By.xpath("//span[contains(text(),'7')]"));
+        WebElement K8 = getDriver().findElement(By.xpath("//span[contains(text(),'8')]"));
+        WebElement K9 = getDriver().findElement(By.xpath("//span[contains(text(),'9')]"));
+        WebElement Kdp = getDriver().findElement(By.xpath("//span[contains(text(),'.')]"));
+
+        for (int i = 0; i < nbr2.length(); i++){
+            char c = nbr2.charAt(i);
+            String key = String.valueOf(c);
+
+            switch (key) {
+                case "0" -> K0.click();
+                case "1" -> K1.click();
+                case "2" -> K2.click();
+                case "3" -> K3.click();
+                case "4" -> K4.click();
+                case "5" -> K5.click();
+                case "6" -> K6.click();
+                case "7" -> K7.click();
+                case "8" -> K8.click();
+                case "9" -> K9.click();
+                case "." -> Kdp.click();
+                default -> throw new IllegalStateException("Error: This key name is invalid: " + key);
+            }
+        }
+    }
+
+    // -----------------------------------------------------------------
+    @And("I press the {string} key")
+    public void iPressTheKey(String keyt) {
+        WebElement Kadd = getDriver().findElement(By.xpath("//span[contains(@onclick,\"r('+')\")]"));
+        WebElement Ksub = getDriver().findElement(By.xpath("//span[contains(@onclick,\"r('-')\")]"));
+        WebElement Kac = getDriver().findElement(By.xpath("//span[contains(text(),'AC')]"));
+        WebElement Keq = getDriver().findElement(By.xpath("//span[contains(text(),'=')]"));
+
+        switch (keyt) {
+            case "+" -> Kadd.click();
+            case "-" -> Ksub.click();
+            case "C" -> Kac.click();
+            case "=" -> Keq.click();
+            default -> throw new IllegalStateException("Error: This key name is invalid: " + keyt);
+        }
+    }
+
+    // -----------------------------------------------------------------
+    //  @calculatorOutlineTests -- verify calculator results
+    // -----------------------------------------------------------------
+    @Then("I verify the calculation as {string}")
+    public void iVerifyTheCalculationAs(String result) {
+        WebElement Kresult = getDriver().findElement(By.xpath("//div[@id='sciOutPut']"));
+        String expValue = result.strip();
+        String actValue = Kresult.getText().strip();
+        System.out.println("================================================");
+        System.out.println(" Expected Value:   " + expValue);
+        System.out.println(" Actual Value:     " + actValue);
+        System.out.println("------------------------------------------------");
+        assertTrue(expValue.equalsIgnoreCase(actValue));
     }
 }
--- src/test/resources/features/calculatorNet.feature
+++ src/test/resources/features/calculatorNet.feature
@@ -18,3 +18,21 @@ Feature: Unit Converter Test Suite
     And I enter "25000" price, "60" months, "4.5" interest, "5000" downpayment, "0" trade-in, "California" state, "7" percent tax, "300" fees
     And I calculate
     Then I verify monthly pay is "$372.86"
+
+  @calculatorOutlineTests
+  Scenario Outline: Scientific Calculator Tests
+    Given Tool that goes to the "calculator" page
+    When Tool to change resolution to "default"
+    When Tool to get "calculator" test data from source "default"
+    When I start their scientific calculator
+    And I enter first number <nbr1>
+    And I enter the function <fnct1>
+    And I enter the second number <nbr2>
+    And I press the <keyt> key
+    Then I verify the calculation as <result>
+    Examples:
+      | nbr1   | fnct1 | nbr2    | keyt | result   |
+      | "1.0"  | "+"   | "2.0"   | "="  | "3"      |
+      | "5.6"  | "+"   | "2.3"   | "="  | "7.9"    |
+      | "5.69" | "-"   | "2.3"   | "="  | "3.39"   |
+      | "5.69" | "-"   | "10.69" | "="  | "-5"     |
