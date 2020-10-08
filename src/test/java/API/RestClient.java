4d43f39bca69e59a87981ec8c64735270c0f3dfd
paulc1600 <ctmtest2304@gmail.com>
2020-10-08 14:56:15 -0700

    Day 19 REST code complete and running

---
+++ src/test/java/API/RestClient.java
@@ -0,0 +1,303 @@
+package API;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static support.TestContext.*;
+
+public class RestClient {
+
+    private String baseUrl = "https://skryabin.com/recruit/api/v1/";
+    private static String loginToken;
+    public static final String CONTENT_TYPE = "Content-Type";
+    public static final String JSON = "application/json";
+    public static final String AUTH = "Authorization";
+
+    // --------------------------------------------------------------
+    //  POST https://skryabin.com/recruit/api/v1/login
+    //     {
+    //        "email": "owen@example.com",
+    //        "password": "welcome"
+    //     }
+    //   returns
+    //     {
+    //         "authenticated": true,
+    //         "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im9
+    //              YXQiOjE2MDI.....blah, blah,"
+    //         "issuedAt": 1602100131,
+    //         "expiresAt": 1602186531
+    //    }
+    // --------------------------------------------------------------
+    public void login(Map<String, String> user) {
+        // prepare -- header tells server how to parse body -- lookup header detail in Postman Header
+        RequestSpecification request = RestAssured.given()
+                .log().all()
+                .baseUri(baseUrl)
+                .basePath("login")
+                .header(CONTENT_TYPE, JSON)
+                .body(user);
+
+        // execute -- need use post (look in swagger)
+        Response response = request.when().post();
+
+        // verify and extract data
+        Map<String, Object> result = response.then()
+                .log().all()
+                .statusCode(200)
+                .extract()
+                .jsonPath()
+                .getMap("");
+
+        loginToken = "Bearer " + result.get("token");
+        System.out.println(loginToken);
+    }
+
+    // --------------------------------------------------------------
+    //  POST https://skryabin.com/recruit/api/v1/positions
+    //    {
+    //            "title": "Senior Unknown Analyst",
+    //            "address": "1234 Fog Way",
+    //            "city": "San Jose",
+    //            "state": "CA",
+    //            "zip": "95123",
+    //            "description": "Help our company to be even more confused than it already is.",
+    //            "dateOpen": "2020-04-28",
+    //            "company": "Unknown Working Environment"
+    //    }
+    //   returns
+    //    {
+    //            "title": "Senior Unknown Analyst",
+    //            "address": "1234 Fog Way",
+    //            "city": "San Jose",
+    //            "state": "CA",
+    //            "zip": "95123",
+    //            "description": "Help our company to be even more confused than it already is.",
+    //            "dateOpen": "2020-04-28",
+    //            "company": "Unknown Working Environment",
+    //            "id": 2827
+    //    }
+    // --------------------------------------------------------------
+    public void createPosition(Map<String, String> position) {
+        String title = position.get("title");
+        position.put("title", title + getTimestamp());
+
+        // prepare
+        RequestSpecification request = RestAssured.given()
+                .log().all()
+                .baseUri(baseUrl)
+                .basePath("positions")
+                .header(CONTENT_TYPE, JSON)
+                .header(AUTH, loginToken)
+                .body(position);
+
+        // execute
+        Response response = request.when()
+                .post();
+
+        // verify and extract
+        Map<String, Object> result = response.then()
+                .log().all()
+                .statusCode(201)
+                .extract()
+                .jsonPath()
+                .getMap("");
+
+        setTestData("newPosition", result);
+        setTestData("newPositionID", result.get("id"));
+    }
+
+    // --------------------------------------------------------------
+    //  GET https://skryabin.com/recruit/api/v1/positions
+    //   returns
+    //   [
+    //    {
+    //        "id": 2834,
+    //            "title": "Super Senior QA Automation",
+    //            "address": "308 West Way",
+    //            "city": "San Francisco",
+    //            "state": "CA",
+    //            "zip": "94099",
+    //            "description": "Must have 7 years experience in Python, Java, C, Cobol",
+    //            "dateOpen": "2020-12-05",
+    //            "company": "Blue Corncobs",
+    //            "candidatesCount": 0
+    //    },
+    // --------------------------------------------------------------
+    public List<Map<String, Object>> getPositionList(int posID, boolean shouldBeInList) {
+        // prepare
+        RequestSpecification request = RestAssured.given()
+                .log().all()
+                .baseUri(baseUrl)
+                .basePath("positions");
+
+        // execute
+        Response response = request.when()
+                .get();
+
+        List <Map<String, Object>> result = response.then()
+                .log().all()
+                .statusCode(200)
+                .extract()
+                .jsonPath()
+                .getList("");
+
+        // Check all positions in Response Body List -- for position just created
+        boolean isInList = false;
+        String listedPosID = "";
+        for (Map<String, Object> positionOBJ : result) {
+            listedPosID = String.valueOf(positionOBJ.get("id"));
+            System.out.println("List Position = " + listedPosID + " compare " + posID);
+            if (listedPosID.equals(""+posID)) {
+                isInList = true;
+                break;
+            }
+        }
+        if (shouldBeInList) {
+            System.out.println("=========================================");
+            System.out.println(" Target Record ID: " + posID);
+            System.out.println(" Found Record ID:  " + listedPosID);
+            System.out.println(" Was in list?      " + isInList);
+            System.out.println("=========================================");
+            assertThat(isInList);
+        } else {
+            System.out.println("=========================================");
+            System.out.println(" Target Record ID: " + posID);
+            System.out.println(" Was in list?      " + isInList);
+            System.out.println("=========================================");
+            assertThat(isInList).isFalse();
+        }
+        return result;
+    }
+
+    // --------------------------------------------------------------
+    //  PUT https://skryabin.com/recruit/api/v1/positions/2887
+    //    {
+    //            "title": "Senior Unknown Analyst",
+    //            "address": "1234 Fog Way",
+    //            "city": "San Jose",
+    //            "state": "CA",
+    //            "zip": "95123",
+    //            "description": "Help our company to be even more confused than it already is.",
+    //            "dateOpen": "2020-04-28",
+    //            "company": "Unknown Working Environment"
+    //    }
+    //   returns
+    // --------------------------------------------------------------
+    public Map<String, Object> updatePosition(Map<String, String> position) {
+        String basePath = "positions/" +  getTestDataInteger("newPositionID").toString();
+        String expectedAddress = "258 Very Modified St";
+        String expectedCity = "Modified Village";
+
+        // Needed for compare to actual record get request
+        setTestData("updatedAddress", expectedAddress);
+        setTestData("updatedCity", expectedCity);
+
+        position.put("address", expectedAddress);
+        position.put("city", expectedCity);
+
+        // prepare
+        RequestSpecification request = RestAssured.given()
+                .log().all()
+                .baseUri(baseUrl)
+                .basePath(basePath)
+                .header(CONTENT_TYPE, JSON)
+                .header(AUTH, loginToken)
+                .body(position);
+
+        // execute
+        Response response = request.when()
+                .put();
+
+        // verify and extract
+        Map<String, Object> result = response.then()
+                .log().all()
+                .statusCode(200)
+                .extract()
+                .jsonPath()
+                .getMap("");
+
+        String actualAddress = result.get("address").toString();
+        String actualCity = result.get("city").toString();
+
+        // Can verify that response itself has updated fields
+        assertThat(actualAddress.equalsIgnoreCase(expectedAddress));
+        assertThat(actualCity.equalsIgnoreCase(expectedCity));
+        return result;
+    }
+
+    // --------------------------------------------------------------
+    //  GET https://skryabin.com/recruit/api/v1/positions/2887
+    //   returns
+    //    {
+    //            "title": "Senior Unknown Analyst",
+    //            "address": "1234 Fog Way",
+    //            "city": "San Jose",
+    //            "state": "CA",
+    //            "zip": "95123",
+    //            "description": "Help our company to be even more confused than it already is.",
+    //            "dateOpen": "2020-04-28",
+    //            "company": "Unknown Working Environment"
+    //    }
+    // --------------------------------------------------------------
+    public void verifyPositionUpdates(int posID) {
+        String basePath = "positions/" + posID;
+
+        // prepare
+        RequestSpecification request = RestAssured.given()
+                .log().all()
+                .baseUri(baseUrl)
+                .basePath(basePath);
+
+        // execute
+        Response response = request.when()
+                .get();
+
+        // verify and extract
+        Map<String, Object> result = response.then()
+                .log().all()
+                .statusCode(200)
+                .extract()
+                .jsonPath()
+                .getMap("");
+
+        String actualAddress = result.get("address").toString();
+        String actualCity = result.get("city").toString();
+
+        // Can verify that response itself has updated fields
+        assertThat(actualAddress.equalsIgnoreCase(getTestDataString("updatedAddress")));
+        assertThat(actualCity.equalsIgnoreCase(getTestDataString("updatedCity")));
+    }
+
+    // --------------------------------------------------------------
+    //  DELETE https://skryabin.com/recruit/api/v1/positions/2887
+    //   returns
+    //     204 = Successfully deleted
+    // --------------------------------------------------------------
+    public void deletePositionRecord(int posID) {
+        String basePath = "positions/" + posID;
+
+        // prepare
+        RequestSpecification request = RestAssured.given()
+                .log().all()
+                .baseUri(baseUrl)
+                .basePath(basePath)
+                .header(AUTH, loginToken);
+
+        // execute
+        Response response = request.when()
+                .delete();
+
+        // verify successful completion
+        String result = response.then()
+                .log().all()
+                .statusCode(204)
+                .extract()
+                .asString();
+        System.out.println("Delete Result Body: " + result);
+    }
+}
--- src/test/java/definitions/RestStepDefs.java
+++ src/test/java/definitions/RestStepDefs.java
@@ -1,13 +1,18 @@
 package definitions;
 
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import support.RestClient;
+import API.RestClient;
 
 import static definitions.ATestToolBox.*;
 import static definitions.ATestToolBox.tePageURL;
+import static support.TestContext.getTestDataInteger;
+import static support.TestContext.setTestData;
 
 public class RestStepDefs {
+    RestClient RC = new RestClient();
+
     @Given("I open rest environment for {string}")
     public void iOpenRestEnvironmentFor(String url) {
         String title = "";
@@ -34,26 +39,57 @@ public class RestStepDefs {
 
     // --------------------------------------------------------------
     //  POST https://skryabin.com/recruit/api/v1/login
-    //     {
-    //        "email": "owen@example.com",
-    //        "password": "welcome"
-    //     }
-    //   returns
-    //     {
-    //         "authenticated": true,
-    //         "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im9
-    //              YXQiOjE2MDI.....blah, blah,"
-    //         "issuedAt": 1602100131,
-    //         "expiresAt": 1602186531
-    //    }
     // --------------------------------------------------------------
     @Given("I login via REST as {string}")
     public void iLoginViaRESTAs(String role) {
-        new RestClient().login(getStrData(role));
+        RC.login(getStrData(role));
     }
 
+    // --------------------------------------------------------------
+    //  POST https://skryabin.com/recruit/api/v1/positions
+    // --------------------------------------------------------------
     @When("I create via REST {string} position")
     public void iCreateViaRESTPosition(String type) {
-        new RestClient().createPosition(getStrData(type));
+        RC.createPosition(getStrData(type));
+    }
+
+    // --------------------------------------------------------------
+    //  GET https://skryabin.com/recruit/api/v1/positions
+    // --------------------------------------------------------------
+    @Then("I verify via REST new position is in the list")
+    public void iVerifyViaRESTNewPositionIsInTheList() {
+        int posID = getTestDataInteger("newPositionID");
+        RC.getPositionList(posID, true);
+    }
+
+    // --------------------------------------------------------------
+    @When("I update via REST {string} position")
+    public void iUpdateViaRESTPosition(String providedPosition) {
+        RC.updatePosition(getStrData(providedPosition));
+    }
+
+    // --------------------------------------------------------------
+    @Then("I verify via REST new position is updated")
+    public void iVerifyViaRESTNewPositionIsUpdated() {
+        // DEPENDENCY: must be set into TestData object by RestClient.createPosition()
+        int posID = getTestDataInteger("newPositionID");
+        RC.verifyPositionUpdates(posID);
+    }
+
+    // --------------------------------------------------------------
+    @When("I delete via REST new position")
+    public void iDeleteViaRESTNewPosition() {
+        // DEPENDENCY: must be set into TestData object by RestClient.createPosition()
+        int posID = getTestDataInteger("newPositionID");
+        RC.deletePositionRecord(posID);
+    }
+
+    // --------------------------------------------------------------
+    @Then("I verity via REST new position is deleted")
+    public void iVerityViaRESTNewPositionIsDeleted() {
+        int posID = getTestDataInteger("newPositionID");
+        RC.getPositionList(posID, false);
+        // Clean up for next time
+        setTestData("newPositionID", "");
     }
 }
